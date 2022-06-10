766de39e9c17ba539aa723574f15be08dd6c0916
Conor Smalley <conor.smalley@outlook.com>
2022-06-10 11:15:00 +0100

    Scaffolded Student model and associated views

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :students
   root 'pages#index'
   get 'pages/index'
   devise_for :users
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_06_10_070049) do
+ActiveRecord::Schema.define(version: 2022_06_10_093437) do
+
+  create_table "students", force: :cascade do |t|
+    t.string "first_name"
+    t.string "last_name"
+    t.date "dob"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
